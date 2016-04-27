f1648ceb53277bd6929ca56122396cb6f8b34b72
Eruk347 <kristian_kure@yahoo.dk>
2016-04-27 15:03:08 +0200

    lille opsætning af kunder

--- ServiceDesk/ServiceDesk/Sider/Kunder.aspx.cs
+++ ServiceDesk/ServiceDesk/Sider/Kunder.aspx.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
+using System.Data;
+using System.Text;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Web.UI.WebControls;
 
 namespace ServiceDesk
@@ -11,7 +11,69 @@ namespace ServiceDesk
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!this.IsPostBack)
+            {
+                //Populating a DataTable from database.
+                DataTable dt = this.GetData();
 
+                //Building an HTML string.
+                StringBuilder html = new StringBuilder();
+
+                //Table start.
+                html.Append("<table border = '1'>");
+
+                //Building the Header row.
+                html.Append("<tr>");
+                foreach (DataColumn column in dt.Columns)
+                {
+                    html.Append("<th>");
+                    html.Append(column.ColumnName);
+                    html.Append("</th>");
+                }
+                html.Append("</tr>");
+
+                //Building the Data rows.
+                foreach (DataRow row in dt.Rows)
+                {
+                    html.Append("<tr>");
+                    foreach (DataColumn column in dt.Columns)
+                    {
+                        html.Append("<td>");
+                        html.Append(row[column.ColumnName]);
+                        html.Append("</td>");
+                    }
+                    html.Append("</tr>");
+                }
+
+                //Table end.
+                html.Append("</table>");
+
+                //Append the HTML string to Placeholder.
+                //KundeTabel.Controls.Add(new Literal { Text = html.ToString() });
+            }
+        }
+
+        private DataTable GetData()//skal rettes til med ip og login. Evt kalde stored precedure
+        {
+            /*string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(constr))
+            {
+                using (SqlCommand cmd = new SqlCommand("SELECT CustomerId, Name, Country FROM Customers"))
+                {
+                    using (SqlDataAdapter sda = new SqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            sda.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            */
+            DataTable dt = new DataTable();
+            return dt;
         }
     }
 }
