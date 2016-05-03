bea962297ae103e0cdec9a16a86c7bc0b8067d1f
Ruben2776 <ruben@live.dk>
2016-05-03 09:28:40 +0200

    mvc routing startup

---
+++ ServiceDesk/ServiceDesk/App_Code/db.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ServiceDesk.App_Code
+{
+    public static class db
+    {
+        private const string DB_CONN = "";
+    }
+}
---
+++ ServiceDesk/ServiceDesk/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ServiceDesk.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kunder()
+        {
+            return View();
+        }
+
+        public ActionResult Maskintyper()
+        {
+            return View();
+        }
+    }
+}
