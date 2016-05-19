3cb805b5f3a982ab545c638e4aaeae8556ba4cbe
Ruben2776 <ruben@live.dk>
2016-05-19 07:54:39 +0200

    Improved mysql db code

--- ServiceDesk/ServiceDesk/Controllers/DefaultController.cs
+++ ServiceDesk/ServiceDesk/Controllers/DefaultController.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using ServiceDesk;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,37 +10,10 @@ namespace ServiceDesk.Controllers
 {
     public class DefaultController : Controller
     {
-        private const string DB_CONN = "server=192.168.1.253;uid=root;pwd=Passw0rd;database=servicedesk;";
-
-        public string SafeGetString(MySqlDataReader reader, int colIndex)
-        {
-            if (!reader.IsDBNull(colIndex))
-                return reader.GetString(colIndex);
-            else
-                return string.Empty;
-        }
-
         public ActionResult Index()
         {
-            var conn = new MySqlConnection();
-            var cmd = new MySqlCommand("SELECT * FROM `bruger` LIMIT 0 , 30", conn);
-            conn.ConnectionString = DB_CONN;
-            conn.Open();
-            var reader = cmd.ExecuteReader();
-            int i = 0;
-            var users = new List<string>();
-            while (reader.Read())
-            {
-                users.Add(SafeGetString(reader, i));
-                users.Add(SafeGetString(reader, i + 1));
-                users.Add(SafeGetString(reader, i + 2));
-                users.Add(SafeGetString(reader, i + 3));
-                users.Add(SafeGetString(reader, i + 4));
-                users.Add(SafeGetString(reader, i + 5));
-                users.Add(SafeGetString(reader, i + 6));
-                i++;
-            }
-            ViewBag.users = users;
+
+            ViewBag.users = db.GetValuesAsStrings("SELECT `id`, `fornavn`, `password`, `telefon`, `type`, `email` FROM `bruger` LIMIT 0, 100", 6);
 
 
             return View();
