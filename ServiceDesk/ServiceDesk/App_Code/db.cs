482aa06ae1d844f33dddd9e33df0584f166c5d0c
Eruk347 <kristian_kure@yahoo.dk>
2016-05-30 14:26:14 +0200

    Functions start update

--- ServiceDesk/ServiceDesk/App_Code/db.cs
+++ ServiceDesk/ServiceDesk/App_Code/db.cs
@@ -62,17 +62,5 @@ namespace ServiceDesk
             conn.Open();
             cmd.ExecuteNonQuery();
         }
-
-        public static void CreateUser(string data)
-        {
-            string SQLComm = "Insert into bruger (fornavn,efternavn,password,telefon,type,email) VALUES (" + data + ")";
-            db.SQLNonQuery(SQLComm);
-        }
-
-        public static void Deleteuser(int id)
-        {
-            string SQLComm = "Delete from brugere where id=" + id;
-            db.SQLNonQuery(SQLComm);
-        }
     }
 }
