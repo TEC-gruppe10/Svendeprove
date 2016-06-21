6acde1205a9256fd2f268e3fcb4a695e82478645
Ruben2776 <ruben@live.dk>
2016-06-21 08:01:19 +0200

    cleanup

--- ServiceDesk/ServiceDesk/App_Code/db.cs
+++ ServiceDesk/ServiceDesk/App_Code/db.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 
 namespace ServiceDesk
@@ -10,6 +11,14 @@ namespace ServiceDesk
     {
         public const string DB_CONN = "server=192.168.1.253;uid=root;pwd=Passw0rd;database=servicedesk;";
 
+
+        #region SafeGetString
+        /// <summary>
+        /// Used to safely get string values from sql data reader. Returns empty strings if null.
+        /// </summary>
+        /// <param name="reader"></param>
+        /// <param name="colIndex"></param>
+        /// <returns></returns>
         public static string SafeGetString(MySqlDataReader reader, int colIndex)
         {
             if (!reader.IsDBNull(colIndex))
@@ -17,7 +26,13 @@ namespace ServiceDesk
             else
                 return string.Empty;
         }
+        #endregion
 
+        #region DefaultConnection
+        /// <summary>
+        /// Used to establish a default connection
+        /// </summary>
+        /// <returns></returns>
         private static MySqlConnection DefaultConnection()
         {
             var conn = new MySqlConnection();
@@ -25,7 +40,16 @@ namespace ServiceDesk
 
             return conn;
         }
+        #endregion
 
+        #region GetValuesAsStrings
+        /// <summary>
+        /// Used to extract values from db as strings. Null values will be empty strings.
+        /// Need a column count to work.
+        /// </summary>
+        /// <param name="sqlcmd"></param>
+        /// <param name="columns"></param>
+        /// <returns></returns>
         public static List<string[]> GetValuesAsStrings(string sqlcmd, int columns)
         {            
             using (var conn = DefaultConnection())
@@ -54,13 +78,88 @@ namespace ServiceDesk
             }
         }
 
+        public async static Task<List<string[]>> GetValuesAsStringsAsync(string sqlcmd, int columns)
+        {
+            using (var conn = DefaultConnection())
+            {
+                var cmd = new MySqlCommand(sqlcmd, conn);
+                await conn.OpenAsync();
+
+                var reader = cmd.ExecuteReader();
+                if (!reader.HasRows)
+                    return null;
+
+                var list = new List<string[]>();
+
+                while (await reader.ReadAsync())
+                {
+                    var array = new string[columns];
+                    for (int i = 0; i < columns; i++)
+                    {
+                        array[i] = SafeGetString(reader, i);
+                    }
+                    list.Add(array);
+                }
+
+                reader.Close();
+                return list;
+            }
+        }
+        #endregion
+
+        #region SQLNonQuery
+        /// <summary>
+        /// Executes a query that does not return results
+        /// </summary>
+        /// <param name="sqlcmd"></param>
         public static void SQLNonQuery(string sqlcmd)
         {
-            var conn = new MySqlConnection();
-            var cmd = new MySqlCommand(sqlcmd, conn);
-            conn.ConnectionString = DB_CONN;
-            conn.Open();
-            cmd.ExecuteNonQuery();
+            using (var conn = DefaultConnection())
+            {
+                conn.Open();
+                var cmd = new MySqlCommand(sqlcmd, conn);
+                cmd.ExecuteNonQuery();
+            }
+            
         }
+        #endregion
+
+        #region CheckUser
+        public static int CheckUser(string email, string password)
+        {
+            var sqlcmd = "SELECT type FROM bruger WHERE email = '";
+            sqlcmd += email;
+            sqlcmd += "' and password = '";
+            sqlcmd += password;
+            sqlcmd += "' LIMIT 0,1";
+
+            using (var conn = DefaultConnection())
+            {
+                conn.Open();
+                var cmd = new MySqlCommand(sqlcmd, conn);
+                var reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    return reader.GetUInt16(0) == 0 ? 1 : 2;
+                }
+                else
+                {
+                    sqlcmd = "SELECT id FROM kunder WHERE email = '";
+                    sqlcmd += email;
+                    sqlcmd += "' and password = '";
+                    sqlcmd += password;
+                    sqlcmd += "' LIMIT 0,1";
+                    reader.Close();
+                    cmd.Dispose();
+                    reader.Dispose();
+                    cmd = new MySqlCommand(sqlcmd, conn);
+                    reader = cmd.ExecuteReader();
+                    return reader.HasRows ? 3 : 0;
+                }
+            }
+        }
+        #endregion
+
+
     }
 }
